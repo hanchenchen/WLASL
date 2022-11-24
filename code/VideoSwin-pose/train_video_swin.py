55cafb35d034f876f73b9e8525c5aa2669947c3e
Chenchen <13980209828@163.com>
2022-11-24 11:24:52 +0800

    1124-31-wo-scheduler-29

--- code/VideoSwin-pose/train_video_swin.py
+++ code/VideoSwin-pose/train_video_swin.py
@@ -240,7 +240,7 @@ def run(configs,
                 os.remove(path)
                 print('Remove:', path)
             print(model_name)
-        scheduler.step(val_score_dict['val_loss'] * num_steps_per_update / num_iter)
+        # scheduler.step(val_score_dict['val_loss'] * num_steps_per_update / num_iter)
 
         localtime = datetime.datetime.fromtimestamp(
             int(time.time()), pytz.timezone("Asia/Shanghai")
@@ -264,7 +264,7 @@ if __name__ == '__main__':
     # root = {'word': '/raid_han/sign-dataset/wlasl/videos'}
     root = {'word': '/raid_han/signDataProcess/capg-csl-resized'}
 
-    save_model = '1123-29-debug-scheduler-max_min-28'
+    save_model = '1124-31-wo-scheduler-29'
     os.makedirs(save_model, exist_ok=True)
     train_split = 'preprocess/nslt_100.json'
 
