2c4ef2ea6839988ee03e9d8aa3c6bd6258fe6120
Chenchen <13980209828@163.com>
2022-12-06 20:02:46 +0800

    1206-98-wo-unimodal-cls-88

--- code/VideoSwin-pose/train_video_swin.py
+++ code/VideoSwin-pose/train_video_swin.py
@@ -2,7 +2,7 @@ import os
 import argparse
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = '6'
+os.environ["CUDA_VISIBLE_DEVICES"] = '7'
 device_ids = [0]
 import torch
 import torch.nn as nn
@@ -101,7 +101,7 @@ def run(configs,
     cue = ['full_rgb', 'right_hand', 'left_hand', 'face', 'pose']
     # cue = ['face']
     # supervised_cue = cue + ['multi_cue', 'late_fusion']
-    supervised_cue = cue + ['late_fusion'] + [f'local_align/{i}' for i in cue] + ['local_glocal_fusion']
+    supervised_cue = ['late_fusion'] + [f'local_align/{i}' for i in cue] + ['local_glocal_fusion']
     # supervised_cue = cue + ['late_fusion'] 
     model = MultiCueModel(cue, num_classes, share_hand_model=False)
 
@@ -353,7 +353,7 @@ if __name__ == '__main__':
     root = {'word': ['/raid_han/signDataProcess/capg-csl-dataset/capg-csl-1-20', '/raid_han/signDataProcess/capg-csl-dataset/capg-csl-21-100'], 'train': ['maodonglai'], 'test': ['liya']}
     # root = {'word': ['/raid_han/signDataProcess/capg-csl-dataset/capg-csl-1-20']}
 
-    save_model = f'logdir/train_{root["train"][0]}/1204-88-sum-all-logits-87'
+    save_model = f'logdir/train_{root["train"][0]}/1206-98-wo-unimodal-cls-88'
     os.makedirs(save_model, exist_ok=True)
     train_split = 'preprocess/nslt_100.json'
 
