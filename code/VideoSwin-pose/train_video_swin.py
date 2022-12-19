ad887e3a2f5ced85fae9f955d0a54009475ec841
Chenchen <13980209828@163.com>
2022-12-19 18:09:09 +0800

    1219-03-wo-m-align-01

--- code/VideoSwin-pose/train_video_swin.py
+++ code/VideoSwin-pose/train_video_swin.py
@@ -2,7 +2,7 @@ import os
 import argparse
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = '3'
+os.environ["CUDA_VISIBLE_DEVICES"] = '4'
 device_ids = [0]
 import torch
 import torch.nn as nn
@@ -102,8 +102,7 @@ def run(configs,
     # cue = ['full_rgb', 'right_hand', 'left_hand', 'face', 'pose']
     cue = ['full_rgb', 'right_hand', 'left_hand', 'face', 'pose']
     # supervised_cue = cue + ['late_fusion', 'local_align/multimodal'] + [f'local_align/{i}' for i in cue] + ['local_glocal_fusion']
-    # supervised_cue = cue + ['local_align/multimodal'] + [f'local_align/{i}' for i in cue] + ['local_glocal_fusion']
-    supervised_cue = cue + ['local_align/multimodal'] + [f'local_align/{i}' for i in cue] + ['local_glocal_fusion']
+    supervised_cue = cue + ['late_fusion'] + [f'local_align/{i}' for i in cue] + ['local_glocal_fusion']
     model = MultiCueModel(cue, supervised_cue, num_classes, share_hand_model=False)
 
     if weights:
@@ -392,7 +391,7 @@ def train_(root, save_model, weights):
 
 if __name__ == '__main__':
 
-    exp_name = '1219-02-wo-m-cls-01'
+    exp_name = '1219-03-wo-m-align-01'
 
     weights = None
     root = {'word': ['/raid_han/signDataProcess/capg-csl-dataset/capg-csl-1-20', '/raid_han/signDataProcess/capg-csl-dataset/capg-csl-21-100'], 'train': ['liya'], 'test': ['maodonglai']}
