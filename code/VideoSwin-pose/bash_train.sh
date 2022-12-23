e9b68c41e2109fd306aac44b391e745f2c81df2b
Chenchen <13980209828@163.com>
2022-12-24 01:27:32 +0800

    1223-188=179

--- code/VideoSwin-pose/bash_train.sh
+++ code/VideoSwin-pose/bash_train.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=5 nohup python train_video_swin.py > logdir/logs/1223-187=163-output 2>&1 &  \
+CUDA_VISIBLE_DEVICES=3 nohup python train_video_swin.py > logdir/logs/1223-188=179-output 2>&1 &  \
 # --load-weights "work-dir/dist-gpu=2xbs=1-train/1009-21-wo-SimsiamAlign-18 18.9/dev_0.18900_epoch38_model.pt" & \
 # --phase test  & \
 # --work-dir work-dir/test/22-backward_hook-21-dist-bs=2-gpu=1-02/
